701cfa87e16bb9a44980e97bc2f9bdd23be25c4f
eN1nja <mr.buntar@gmail.com>
2023-07-25 06:51:46 +0300

    исправил код в задаче 10 с использования строковых операций для получения цифр из числа

--- Задача_10/Program.cs
+++ Задача_10/Program.cs
@@ -5,14 +5,36 @@
 // 918 -> 1
 
 Console.Write("Введите трёхзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-string numText = Convert.ToString(num);
-
-if (numText.Length < 3 || numText.Length > 3)
+string? value = Console.ReadLine();
+int result;
+if (int.TryParse(value, out result))
 {
-    Console.WriteLine("Проверьте ввод! Введите трёхзначное число!");
+    if (result >= 100 && result <= 999)
+    {
+        result = (result / 10) % 10;
+        Console.Write($"Вторая цифра числа = {result}");
+    }
+    else
+    {
+        Console.WriteLine("Проверьте кол-во цифр ввода! Введите трёхзначное число!");
+    }
 }
 else
 {
-    Console.Write("Вторая цифра числа =  " + numText[1]);
+    Console.WriteLine("Ошибка! Введите трёхзначное число!");
 }
+
+
+// v2 в работе используются строковые операции для получения цифр из числа
+// Console.Write("Введите трёхзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+
+// if (numText.Length < 3 || numText.Length > 3)
+// {
+//     Console.WriteLine("Проверьте ввод! Введите трёхзначное число!");
+// }
+// else
+// {
+//     Console.Write("Вторая цифра числа =  " + numText[1]);
+// }
